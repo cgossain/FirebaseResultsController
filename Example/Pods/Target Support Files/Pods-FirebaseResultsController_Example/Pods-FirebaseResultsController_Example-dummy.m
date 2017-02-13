de39ca2a7c49819b83c3596bd995f287baa91804
Christian Gossain <cgossain@gmail.com>
2017-02-12 17:52:28 -0700

    Initial pod setup.

---
+++ Example/Pods/Target Support Files/Pods-FirebaseResultsController_Example/Pods-FirebaseResultsController_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FirebaseResultsController_Example : NSObject
+@end
+@implementation PodsDummy_Pods_FirebaseResultsController_Example
+@end
